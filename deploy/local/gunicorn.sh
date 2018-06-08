5676ed32dc0aa04d78797d04e2155138ac1b0c13
Junn <xiongjun@juyanggroup.com>
2018-06-08 16:21:13 +0800

    update gunicorn.sh

--- deploy/local/gunicorn.sh
+++ deploy/local/gunicorn.sh
@@ -5,12 +5,13 @@ TIMEOUT=300   #to solve upload app package timeout issue
 cd /home/deploy/nmis/prod/nmis-back/apps
 
 # activate the virtualenv
-workon nmis
+# workon nmis
+source /home/deploy/.virtualenvs/nmis/bin/activate
 
 # changed:  --bind 0.0.0.0:8002 to --bind 127.0.0.1:8002, forbid 8002 port access
 exec gunicorn wsgi -w 2 \
     --bind 127.0.0.1:8002 \
-    --settings settings.local \
+    --env DJANGO_SETTINGS_MODULE=settings.local \
     --user deploy --group deploy \
     --timeout $TIMEOUT \
     --log-level info
