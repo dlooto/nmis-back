a54e2090b4525cf45088cf133d1738ae6afe1357
zengjiarui <zengjiarui@juyanggroup.com>
2018-10-17 22:21:36 +0800

    修改资产设备serializer

--- apps/nmis/devices/serializers.py
+++ apps/nmis/devices/serializers.py
@@ -56,13 +56,49 @@ class ContractDeviceSerializer(BaseModelSerializer):
 
 
 class AssertDeviceSerializer(BaseModelSerializer):
+
+    performer_name = serializers.SerializerMethodField('_get_performer_name')
+    medical_device_cate_title = serializers.SerializerMethodField('_get_medical_device_cate_title')
+    responsible_dept_name = serializers.SerializerMethodField('_get_responsible_dept_name')
+    use_dept_name = serializers.SerializerMethodField('_get_use_dept_name')
+    storage_place_title = serializers.SerializerMethodField('_get_storage_place_title')
+    creator_name = serializers.SerializerMethodField('_get_creator_name')
+    modifier_name = serializers.SerializerMethodField('_get_modifier_name')
+
     class Meta:
         model = AssertDevice
-        fields = '__all__'
+        fields = ('id', 'created_time', 'creator_id', 'creator_name', 'assert_no', 'title',
+                  'medical_device_cate_id', 'medical_device_cate_title', 'serial_no',
+                  'modifier_id', 'modifier_name', 'type_spec', 'service_life',
+                  'performer_id', 'performer_name', 'responsible_dept_id',
+                  'responsible_dept_name', 'use_dept_id', 'use_dept_name',
+                  'production_date', 'bar_code', 'status', 'storage_place_id',
+                  'storage_place_title', 'purchase_date', 'cate')
 
     @staticmethod
     def setup_eager_loading(queryset):
         pass
+
+    def _get_performer_name(self, obj):
+        return obj.performer.name if obj.performer else ''
+
+    def _get_medical_device_cate_title(self, obj):
+        return obj.medical_device_cate.title if obj.medical_device_cate else ''
+
+    def _get_responsible_dept_name(self, obj):
+        return obj.responsible_dept.name if obj.responsible_dept else ''
+
+    def _get_use_dept_name(self, obj):
+        return obj.use_dept.name if obj.use_dept else ''
+
+    def _get_storage_place_title(self, obj):
+        return obj.storage_place.title if obj.storage_place else ''
+
+    def _get_creator_name(self, obj):
+        return obj.creator.title if obj.creator else ''
+
+    def _get_modifier_name(self, obj):
+        return obj.modifier.title if obj.modifier else ''
 
 
 class MedicalDeviceSix8CateSerializer(BaseModelSerializer):
