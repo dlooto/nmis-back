1c6809ba588e8aff95ca1790a50755fe902a3a35
zengjiarui <zengjiarui@juyanggroup.com>
2018-11-30 14:36:24 +0800

    增加全部已读标记为删除/全部未读标记为已读API接口

--- apps/nmis/notices/views.py
+++ apps/nmis/notices/views.py
@@ -10,6 +10,7 @@ from base.views import BaseAPIView
 from nmis.hospitals.permissions import HospitalStaffPermission
 from nmis.notices.models import Notice, UserNotice
 from nmis.notices.serializers import UserNoticeSerializer
+from utils import times
 
 logger = logging.getLogger(__name__)
 
@@ -31,28 +32,71 @@ class NoticeListView(BaseAPIView):
         return self.get_pages(query_set, results_name='notices')
 
 
-class NoticeView(BaseAPIView):
+class NoticeReadOrDeleteView(BaseAPIView):
 
     permission_classes = (HospitalStaffPermission, )
 
-    @check_params_not_null(['notice_ids'])
-    def delete(self, req):
+    @check_params_not_null(['notice_ids', 'op_type'])
+    def put(self, req):
         """
-        删除消息（批量删除/单个删除）
+        读取消息/删除消息（标记单个/多条消息为删除状态，标记单个/多条消息为已读状态）
+        type: 操作类型（删除操作：DE、读取操作:RE）必传字段
+        notice_ids: 消息ids集合字符串，如："1,2,3" 必传字段
         """
         staff = req.user.get_profile()
-        self.check_object_any_permissions(req, staff)
+        self.check_object_permissions(req, staff)
 
         notice_ids = get_id_list(req.data.get('notice_ids', '').strip())
+
+        operation_type = req.data.get('op_type', '').strip()
+        if operation_type not in ('RE', 'DE'):
+            return resp.failed('不合法的操作类型数据')
 
         user_notices = UserNotice.objects.filter(notice_id__in=notice_ids, staff=staff)
         if not len(notice_ids) == len(user_notices):
             return resp.failed('检查是否存在不匹配的消息')
         try:
-            user_notices.update(is_delete=False)
+            if operation_type == 'RE':
+                if not user_notices.filter(is_read=False):
+                    return resp.failed('消息已读，无法改变消息状态')
+                user_notices.filter(is_read=False).update(is_read=True, read_time=times.now())
+            else:
+                # 选中的消息存在未读的情况下未考虑，直接标记成删除状态，如需考虑，后续改进
+                user_notices.update(is_delete=True, delete_time=times.now())
             return resp.ok('操作成功')
         except Exception as e:
             logger.exception(e)
-            return resp.ok('操作失败')
+            return resp.failed('操作失败')
 
+
+class NoticeReadOrDeleteAllView(BaseAPIView):
+
+    permission_classes = (HospitalStaffPermission, )
+
+    @check_params_not_null(['op_type'])
+    def put(self, req):
+        """
+        op_type: 操作类型（全部标为已读(RE): 所有未读消息标记为已读, 删除全部已读(DE): 所以已读消息标记为删除）
+        根据操作类型把消息标记为相对应的状态
+        """
+        staff = req.user.get_profile()
+        self.check_object_permissions(req, staff)
+
+        operation_type = req.data.get('op_type', '').strip()
+        if operation_type not in ('ARE', 'ADE'):
+            return resp.failed('不合法的操作类型数据')
+
+        try:
+            if operation_type == 'ARE':
+                query_set = UserNotice.objects.filter(staff=staff, is_read=False)
+                if not query_set:
+                    return resp.failed('所有消息都标记为已读状态，无法再次标记')
+                query_set.update(is_read=True, read_time=times.now())
+            else:
+                query_set = UserNotice.objects.filter(staff=staff, is_read=True)
+                query_set.update(is_delete=True, delete_time=times.now())
+            return resp.ok('操作成功')
+        except Exception as e:
+            logger.info(e)
+            return resp.failed('操作失败')
 
